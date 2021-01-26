c1d693fe00c646420deb2363516488cba15a92f3
icaro176 <76911826+icaro176@users.noreply.github.com>
2021-01-26 15:03:23 -0300

    help.js

--- database/menu/help.js
+++ database/menu/help.js
@@ -18,7 +18,9 @@ const help = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
   ├─ ❏ ${prefix}chatlist
   ├─ ❏ ${prefix}ping
   └─ ❏ ${prefix}bugreport
-NOTA: *NEM TODOS OS COMANDOS ESTÃO FUNCIONANDO, MESMO MANDANDO AGUARDAR TALVEZ NÃO FUNCIONE*
+
+NOTA: *NEM TODOS OS COMANDOS ESTÃO FUNCIONANDO, MESMO MANDANDO "AGUARDE" TALVEZ NÃO FUNCIONE*
+
 ◪ *MENU*
   │
   ├─ ❏ ${prefix}logomakermenu
@@ -43,6 +45,192 @@ NOTA: *NEM TODOS OS COMANDOS ESTÃO FUNCIONANDO, MESMO MANDANDO AGUARDAR TALVEZ 
   ├─ ❏ ${prefix}xpmenu
   ├─ ❏ ${prefix}limitmenu
   ├─ ❏ ${prefix}ownermenu
-  └─ ❏ ${prefix}othermenu`
+  └─ ❏ ${prefix}othermenu
+◪ *LOGO MAKER*
+  │
+  ├─ ❏ ${prefix}marvellogo
+  ├─ ❏ ${prefix}ninjalogo
+  ├─ ❏ ${prefix}logowolf
+  ├─ ❏ ${prefix}logowolf2
+  ├─ ❏ ${prefix}phlogo
+  ├─ ❏ ${prefix}neonlogo
+  ├─ ❏ ${prefix}neonlogo2
+  ├─ ❏ ${prefix}lionlogo
+  ├─ ❏ ${prefix}pubglogo
+  └─ ❏ ${prefix}jokerlogo
+◪ *IMAGEM CRIADOR*
+  │
+  ├─ ❏ ${prefix}bpink
+  ├─ ❏ ${prefix}snowwrite
+  ├─ ❏ ${prefix}3dtext
+  ├─ ❏ ${prefix}firetext
+  ├─ ❏ ${prefix}glitch
+  ├─ ❏ ${prefix}shadow
+  ├─ ❏ ${prefix}burnpaper
+  ├─ ❏ ${prefix}coffee
+  ├─ ❏ ${prefix}lovepaper
+  ├─ ❏ ${prefix}woodblock
+  ├─ ❏ ${prefix}qowheart
+  ├─ ❏ ${prefix}mutgrass
+  ├─ ❏ ${prefix}undergocean
+  ├─ ❏ ${prefix}woodenboards
+  ├─ ❏ ${prefix}wolfmetal
+  ├─ ❏ ${prefix}metalictglow
+  ├─ ❏ ${prefix}8bit
+  ├─ ❏ ${prefix}herrypotter
+  └─ ❏ ${prefix}quotemaker
+  └─ ❏ ${prefix}toxic
+  └─ ❏ ${prefix}summer
+  └─ ❏ ${prefix}blood
+  └─ ❏ ${prefix}avengers
+◪ *STICKER MAKER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  └─ ❏ ${prefix}ttp
+◪ *PESQUISA*
+  │
+  ├─ ❏ ${prefix}wait
+  ├─ ❏ ${prefix}ytsearch
+  ├─ ❏ ${prefix}trendtwit
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}wiki
+  └─ ❏ ${prefix}neonime
+◪ *EDUCAÇÃO*
+  │
+  ├─ ❏ ${prefix}wiki
+  ├─ ❏ ${prefix}wikien
+  ├─ ❏ ${prefix}nulis
+  ├─ ❏ ${prefix}quotes
+  ├─ ❏ ${prefix}quotes2
+  └─ ❏ ${prefix}artinama
+◪ *MAKER*
+  │
+  ├─ ❏ ${prefix}apakah
+  ├─ ❏ ${prefix}kapankah
+  ├─ ❏ ${prefix}rate
+  └─ ❏ ${prefix}bisakah
+◪ *DOWNLOAD*
+  │
+  ├─ ❏ ${prefix}pinterest
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  └─ ❏ ${prefix}tiktok
+◪ *MEME*
+  │
+  ├─ ❏ ${prefix}meme
+  └─ ❏ ${prefix}memeindo
+◪ *GRUPO*
+  │
+  ├─ ❏ ${prefix}opengc
+  ├─ ❏ ${prefix}closegc
+  ├─ ❏ ${prefix}promote
+  ├─ ❏ ${prefix}demote
+  ├─ ❏ ${prefix}tagall
+  ├─ ❏ ${prefix}tagall2
+  ├─ ❏ ${prefix}tagall3
+  ├─ ❏ ${prefix}tagall4
+  ├─ ❏ ${prefix}tagall5
+  ├─ ❏ ${prefix}add
+  ├─ ❏ ${prefix}kick
+  ├─ ❏ ${prefix}listadmins
+  ├─ ❏ ${prefix}linkgroup
+  ├─ ❏ ${prefix}leave
+  ├─ ❏ ${prefix}welcome
+  ├─ ❏ ${prefix}nsfw
+  ├─ ❏ ${prefix}delete
+  ├─ ❏ ${prefix}simih
+  └─ ❏ ${prefix}ownergroup
+◪ *SOUND*
+  │
+  └─ ❏ ${prefix}tts
+◪ *MUSIC*
+  │
+  ├─ ❏ ${prefix}play
+  ├─ ❏ ${prefix}joox
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+◪ *ISLAM*
+  │
+  └─ ❏ ${prefix}quran
+◪ *STALK*
+  │
+  ├─ ❏ ${prefix}tiktokstalk
+  └─ ❏ ${prefix}igstalk
+◪ *WIBU*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}husbu
+  ├─ ❏ ${prefix}husbu2
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+◪ *18+*
+  |
+  ├─ ❏ ${prefix}randomhentai
+  ├─ ❏ ${prefix}nsfwtrap
+  └─ ❏ ${prefix}nsfwneko
+◪ *FUN*
+  │
+  ├─ ❏ ${prefix}alay
+  ├─ ❏ ${prefix}gantengcek
+  ├─ ❏ ${prefix}watak
+  ├─ ❏ ${prefix}hobby
+  ├─ ❏ ${prefix}game
+  ├─ ❏ ${prefix}bucin
+  ├─ ❏ ${prefix}trust
+  ├─ ❏ ${prefix}dare
+  └─ ❏ ${prefix}simi
+◪ *TOD*
+  │
+  ├─ ❏ ${prefix}trust
+  └─ ❏ ${prefix}dare
+◪ *INFORMAÇÃO*
+  │
+  ├─ ❏ ${prefix}bahasa
+  ├─ ❏ ${prefix}kodenegara
+  ├─ ❏ ${prefix}kbbi
+  ├─ ❏ ${prefix}fakta
+  ├─ ❏ ${prefix}infocuaca
+  ├─ ❏ ${prefix}infogempa
+  ├─ ❏ ${prefix}jadwaltvnow
+  └─ ❏ ${prefix}covidcountry
+◪ *FIQUE NA TELA*
+  │
+  └─ ❏ ${prefix}afk
+◪ *XP*
+  │
+  ├─ ❏ ${prefix}leveling
+  ├─ ❏ ${prefix}level
+  └─ ❏ ${prefix}mining
+--------------------------------
+Nota: Você pode coletar XP e aumentar seu nível conversando com qualquer pessoa do grupo que tenha ativado o recurso de level.
+◪ *LIMIT*
+  │
+  ├─ ❏ ${prefix}limit
+  ├─ ❏ ${prefix}bal
+  └─ ❏ ${prefix}buylimit
+--------------------------------
+Nota: Você pode comprar limites recolhendo dinheiro primeiro. Verifique seu dinheiro digitando comandos *${prefix}bal* , preço 1 limit = 1000 dinheiro.
+◪ *PROPRIETÁRIO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+◪ *OUTROS*
+  │
+  ├─ ❏ ${prefix}send
+  ├─ ❏ ${prefix}wame
+  ├─ ❏ ${prefix}virtex
+  ├─ ❏ ${prefix}qrcode
+  ├─ ❏ ${prefix}timer
+  ├─ ❏ ${prefix}fml
+  └─ ❏ ${prefix}fml2`
 }
 exports.help = help
